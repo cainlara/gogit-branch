30a7e9a3984a26b1e3623cdf19633ba450046bd9
Jose Andrés Lara Vecino <cainlara@users.noreply.github.com>
2026-08-05 17:42:35 -0500

    Improving copies (#15)

--- execution/help.go
+++ execution/help.go
@@ -25,6 +25,7 @@ func PrintHelp(withError, withUsage bool) {
 			fmt.Println("delete, del:\t\tList all the branches available to delete.")
 			fmt.Println("batch-delete, bd:\tList all the available branches, allowing you to select multiple branches to delete.")
 			fmt.Println("create, c:\t\tCreate a new branch and switch to it.")
+			fmt.Println("status, st:\t\tShow the repository status and commit tracked/untracked changes.")
 			fmt.Println("version, v:\t\tShow the version of this humble tool.")
 			fmt.Println("help, h:\t\tShow this help.")
 		}
